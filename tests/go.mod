202e842a3f48a65a278f2ae952f0b304e1c56a7b
SusanHex <suehex4027@gmail.com>
2025-10-03 22:58:21 +0000

    Add testing files for experimenting

---
+++ tests/go.mod
@@ -0,0 +1,3 @@
+module github.com/SusanHex/RE-Actor/tests
+
+go 1.25.1
---
+++ tests/main.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello from RE-Actor")
+
+	// _, err := fmt.Println("Hello from RE-Actor")
+	// if err != nil {
+	// }
+}
