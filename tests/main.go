6754f7cd01922711aed44645436479dd4fe7c3c3
SusanHex <suehex4027@gmail.com>
2025-10-03 23:40:03 +0000

    Add stuff to susan.go and call a function from main

--- tests/main.go
+++ tests/main.go
@@ -1,11 +1,5 @@
 package main
 
-import "fmt"
-
 func main() {
-	fmt.Println("Hello from RE-Actor")
-
-	// _, err := fmt.Println("Hello from RE-Actor")
-	// if err != nil {
-	// }
+	susan_testing()
 }
