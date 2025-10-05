68d5b4add4fb778df53994a71b0a2ecbf2c07ab8
Kohi-Kamisama <coffeemail48@gmail.com>
2025-10-05 10:37:06 -0600

    added to functions we could use

--- tests/test_programs/coffee.go
+++ tests/test_programs/coffee.go
@@ -2,9 +2,27 @@ package test_programs
 
 import(
 	"fmt"
+	"regexp"
 )
 
 func CoffeeTest(){
 	
 	fmt.Print("Coffee Test")
+	FindingAll()
+	FindingAllString()
+
+}
+
+func FindingAll() {
+	re := regexp.MustCompile(`foo.?`)
+	fmt.Printf("%q\n", re.FindAll([]byte(`seafood fool`), -1))
+
+}
+
+func FindingAllString() {
+	re := regexp.MustCompile(`a.`)
+	fmt.Println(re.FindAllString("paranormal", -1))
+	fmt.Println(re.FindAllString("paranormal", 2))
+	fmt.Println(re.FindAllString("graal", -1))
+	fmt.Println(re.FindAllString("none", -1))
 }
