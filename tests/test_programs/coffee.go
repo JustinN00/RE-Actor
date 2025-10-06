ac30cfca0ac6d36efa3285faf5413982db315484
Kohi-Kamisama <coffeemail48@gmail.com>
2025-10-06 13:19:18 -0600

    Creating a type switch to check the type

--- tests/test_programs/coffee.go
+++ tests/test_programs/coffee.go
@@ -13,6 +13,14 @@ func CoffeeTest() {
 	teststring := "5.10.2025 17:17:52 [Server Event] Player SueHex left."
 	check := regexp.MustCompile(`Player`)
 	checking := check.FindAllSubmatch([]byte(teststring), -1)
+	testtype := []byte(teststring)
+ 
+	switch any(testtype).(type){
+	case string:
+		fmt.Println("string")
+	case byte:
+		fmt.Println("byte")
+	}
 
 	if len(checking) > 0 {
 		fmt.Print("player found\n")
