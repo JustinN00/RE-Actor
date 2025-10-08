31d8d9e878ad5a56185b8a4f27443582cd328b2e
Kohi-Kamisama <coffeemail48@gmail.com>
2025-10-08 03:38:32 -0600

    Testing varibal type so it'll be a string

--- tests/test_programs/coffee.go
+++ tests/test_programs/coffee.go
@@ -10,25 +10,30 @@ func CoffeeTest() {
 
 	fmt.Print("Coffee Test\n\n")
 
-	teststring := "5.10.2025 17:17:52 [Server Event] Player SueHex left."
+	tstring := "5.10.2025 17:17:52 [Server Event] Player SueHex left."
 	check := regexp.MustCompile(`Player`)
-	checking := check.FindAllSubmatch([]byte(teststring), -1)
-	testtype := []byte(teststring)
+	checking := check.FindAllSubmatch([]byte(tstring), -1)
+	testtype := []byte(tstring)
  
+	var teststring string
 	switch any(testtype).(type){
 	case string:
-		fmt.Println("string")
-	case byte:
-		fmt.Println("byte")
+		fmt.Printf("string\n")
+		teststring = string(testtype)
+	case []byte:
+		fmt.Printf("byte\n")
+		teststring = string(testtype)
+	default:
+		fmt.Printf("type: %T\n\n",testtype)
 	}
+	fmt.Println(teststring)
 
 	if len(checking) > 0 {
-		fmt.Print("player found\n")
+		fmt.Print("\nplayer found\n\n")
 		ev := regexp.MustCompile(`Player (.*?)\.`)
 
 		event := ev.FindStringSubmatch(teststring)
 		eventstring := event[1]
-		fmt.Printf("%T\n\n",eventstring)
 
 		split := strings.Split(eventstring, " "); player, action := split[0], split[1]
 
