8dc7570983eff5749abccfcbf2297dd71c919f77
SusanHex <suehex4027@gmail.com>
2025-10-20 23:32:42 +0000

    Add an example for reading the container logs from Docker

--- tests/test_programs/susan.go
+++ tests/test_programs/susan.go
@@ -3,14 +3,19 @@ package test_programs
 import (
 	"context"
 	"fmt"
-	
+	"io"
+	"log"
+	"time"
+
 	"github.com/docker/docker/api/types/container"
 	"github.com/docker/docker/client"
 )
 
 func SusanTest() {
-	
+
 	fmt.Println("Starting Susan Testing function")
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
 	cli, err := client.NewClientWithOpts(client.FromEnv)
 	if err != nil {
 		panic(err)
@@ -22,6 +27,21 @@ func SusanTest() {
 	}
 
 	for _, ctr := range containers {
-		fmt.Printf("%s %s\n", ctr.ID, ctr.Image)
+		reader, err := cli.ContainerLogs(ctx, ctr.ID, container.LogsOptions{
+			ShowStdout: true,
+			ShowStderr: true,
+		})
+		if err != nil {
+			log.Fatal(err)
+		}
+		for {
+			message, err := io.ReadAll(reader)
+			if err != nil {
+				log.Fatal(err)
+			} else if len(message) == 0 {
+				break
+			}
+			log.Println(string(message))
+		}
 	}
 }
