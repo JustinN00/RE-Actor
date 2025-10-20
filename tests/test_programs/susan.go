f020989bc933a9e0862e7575544c7f10f2a8aaea
SusanHex <suehex4027@gmail.com>
2025-10-20 23:02:40 +0000

    Add exmple for using the Docker SDK

--- tests/test_programs/susan.go
+++ tests/test_programs/susan.go
@@ -1,8 +1,27 @@
 package test_programs
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+	
+	"github.com/docker/docker/api/types/container"
+	"github.com/docker/docker/client"
+)
 
 func SusanTest() {
-
+	
 	fmt.Println("Starting Susan Testing function")
+	cli, err := client.NewClientWithOpts(client.FromEnv)
+	if err != nil {
+		panic(err)
+	}
+
+	containers, err := cli.ContainerList(context.Background(), container.ListOptions{})
+	if err != nil {
+		panic(err)
+	}
+
+	for _, ctr := range containers {
+		fmt.Printf("%s %s\n", ctr.ID, ctr.Image)
+	}
 }
