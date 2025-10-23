eb3a469a4fb72bbe9df2503b019c960025a1248e
SusanHex <suehex4027@gmail.com>
2025-10-23 04:56:12 +0000

    Add the DiscordWebHook stuct and the post message function, for posting messages using the information stored in the webhook.

---
+++ utils/actions.go
@@ -0,0 +1,28 @@
+package utils
+
+import (
+	"fmt"
+	"log/slog"
+	"net/http"
+	"strings"
+)
+
+type DiscordWebHook struct {
+	id    string
+	token string
+}
+
+func (dwh *DiscordWebHook) post_message(message string) {
+	url := fmt.Sprintf("https://discord.com/api/webhook/%s/%s", dwh.id, dwh.token)
+	body := fmt.Sprintf("{\"content\":\"%s\"}", message)
+	req, err := http.NewRequest("POST", url, strings.NewReader(body))
+	if err != nil {
+		panic(err)
+	}
+	body_content := []byte{}
+	_, err = req.Response.Body.Read(body_content)
+	if err != nil {
+		panic(err)
+	}
+	slog.Debug("%s")
+}
