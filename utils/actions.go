37236b2ddbeb21de71fb23fb79f5dcb35e7af603
SusanHex <suehex4027@gmail.com>
2025-10-24 03:07:55 +0000

    Add a check that will return an error if the message length is larger than the max in DiscordWebHook.PostMessage

--- utils/actions.go
+++ utils/actions.go
@@ -11,6 +11,10 @@ type DiscordWebHook struct {
 }
 
 func (dwh *DiscordWebHook) PostMessage(message string) (*http.Response, error) {
+	// Discord only allows a max of 2000 characters in the content field.
+	if len(message) > 2000 {
+		return nil, fmt.Errorf("Message of %d characters is larger than the max of 2000 allowed for Discord", len(message))
+	}
 	body := fmt.Sprintf("{\"content\":\"%s\"}", message)
 	client := http.Client{}
 	response, err := client.Post(dwh.URL, "Application/json", strings.NewReader(body))
