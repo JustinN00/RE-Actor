bd10a0b12555dfdee8edaf6be0de473e5065fbd1
SusanHex <suehex4027@gmail.com>
2025-10-27 02:10:00 +0000

    Add viper package and instantiate the config

--- main.go
+++ main.go
@@ -1,11 +1,25 @@
 /*
 Copyright © 2025 NAME HERE <EMAIL ADDRESS>
-
 */
 package main
 
-import "github.com/SusanHex/RE-Actor/cmd"
+import (
+	"log/slog"
+
+	"github.com/SusanHex/RE-Actor/config"
+	"github.com/spf13/viper"
+)
 
 func main() {
-	cmd.Execute()
+	app_config := config.Config{}
+	viper_instance := viper.NewWithOptions()
+	viper_instance.BindEnv("pattern")
+	viper_instance.BindEnv("template")
+	viper_instance.BindEnv("action_name")
+	viper_instance.AutomaticEnv()
+	err := viper_instance.UnmarshalExact(&app_config)
+	if err != nil {
+		panic(err)
+	}
+	slog.Info("Current config: ", "Action", app_config.ActionName, "Pattern:", app_config.Pattern, "Template", app_config.Template)
 }
