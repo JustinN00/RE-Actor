41ea191bb088853913b38487b0cccd3c29058610
SusanHex <suehex4027@gmail.com>
2025-10-28 03:51:10 +0000

    Add ContainerName to config.Config struct

--- config/config.go
+++ config/config.go
@@ -1,6 +1,7 @@
 package config
 
 type Config struct {
+	ContainerName  string `mapstructure:"container_name"`
 	Pattern    string `mapstructure:"pattern"`
 	Template   string `mapstructure:"template"`
 	ActionName string `mapstructure:"action_name"`
