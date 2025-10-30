84b44ad3c74be0586be15e925bd94c85c2efc744
SusanHex <suehex4027@gmail.com>
2025-10-30 05:02:38 +0000

    Add docker to go modules, create docker client, find the container with the container name, then print all the logs from the container.

--- main.go
+++ main.go
@@ -4,11 +4,17 @@ Copyright © 2025 NAME HERE <EMAIL ADDRESS>
 package main
 
 import (
+	"context"
 	"fmt"
+	"io"
 	"log/slog"
+	"time"
 
 	"github.com/SusanHex/RE-Actor/config"
 	"github.com/SusanHex/RE-Actor/utils"
+	"github.com/docker/docker/api/types/container"
+	"github.com/docker/docker/api/types/filters"
+	"github.com/docker/docker/client"
 	"github.com/spf13/viper"
 )
 
@@ -24,10 +30,43 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
-	slog.Info("Current config: ", "Container name",app_config.ContainerName, "Action", app_config.ActionName, "Pattern:", app_config.Pattern, "Template", app_config.Template)
+	slog.Info("Current config: ", "Container name", app_config.ContainerName, "Action", app_config.ActionName, "Pattern:", app_config.Pattern, "Template", app_config.Template)
 	action, err := utils.SelectAction(app_config.ActionName)
 	if err != nil {
 		panic(err)
 	}
 	slog.Info(fmt.Sprintf(`Found action "%T"`, action))
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	cli, err := client.NewClientWithOpts(client.FromEnv)
+	if err != nil {
+		panic(err)
+	}
+	container_name_filter := filters.NewArgs(filters.KeyValuePair{Key: "name", Value: app_config.ContainerName})
+
+	containers, err := cli.ContainerList(context.Background(), container.ListOptions{Filters: container_name_filter})
+	if err != nil {
+		panic(err)
+	}
+	if len(containers) == 0 {
+		panic(fmt.Sprintf(`Could not find a container named "%s"`, app_config.ContainerName))
+	}
+	for _, ctr := range containers {
+		reader, err := cli.ContainerLogs(ctx, ctr.ID, container.LogsOptions{
+			ShowStdout: true,
+			ShowStderr: true,
+		})
+		if err != nil {
+			panic(err)
+		}
+		for {
+			message, err := io.ReadAll(reader)
+			if err != nil {
+				panic(err)
+			} else if len(message) == 0 {
+				break
+			}
+			slog.Info(string(message))
+		}
+	}
 }
