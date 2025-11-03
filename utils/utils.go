71a782c17984ae7a006039b0623a4ffe0517d9e4
SusanHex <suehex4027@gmail.com>
2025-11-03 02:18:32 +0000

    Make it so that the utils.SelectAction has access to the `app_config` for action specific options . Also, add the `utils.PerformActionIfMatch` function to perform the regex checking, template expansion, and action `Act` call.

--- utils/utils.go
+++ utils/utils.go
@@ -2,16 +2,31 @@ package utils
 
 import (
 	"fmt"
+	"log/slog"
 
 	"github.com/SusanHex/RE-Actor/actions"
+	"github.com/SusanHex/RE-Actor/config"
 )
 
-func SelectAction(action_name string) (actions.Action, error) {
+func SelectAction(action_name string, app_config *config.Config) (actions.Action, error) {
 
 	switch action_name {
 	case "discord_webhook":
-		return actions.DiscordWebHook{}, nil
+		return actions.DiscordWebHook{URL: app_config.DiscordWebHookURL}, nil
 	default:
 		return nil, fmt.Errorf(`"%s" does not match an action`, action_name)
 	}
 }
+
+func PerformActionIfMatch(app_config *config.Config, action actions.Action, message []byte) error {
+	match_indexes := app_config.CompiledPattern.FindSubmatchIndex(message)
+	if len(match_indexes) == 0 {
+		return nil
+	}
+	result := []byte{}
+	result = app_config.CompiledPattern.Expand(result, []byte(app_config.Template), message, match_indexes)
+	slog.Info("Acting on", "message", string(result))
+	err := action.Act(string(result))
+	return err
+}
+
